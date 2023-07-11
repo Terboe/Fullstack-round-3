9a6060f1a0f1647b815a65070444a718fc25f3a2
Aku Tervonen <aku@Aku-Mac2022M1.kyla.fi>
2023-07-11 19:03:30 +0300

    adding the files

---
+++ pluetBE/index.js
@@ -0,0 +1,93 @@
+
+const http = require('http')
+const express = require('express')
+const morgan = require('morgan')
+const { token } = require('morgan')
+const cors = require('cors')
+
+let persons = [
+      {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": 9948
+      },
+      {
+        "name": "d",
+        "number": "5",
+        "id": 5
+      },
+      {
+        "name": "spankers!",
+        "number": "6",
+        "id": 6
+      }
+]
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(morgan(function (tokens, req, res) {
+  ret = [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms --- '
+  ].join(' ')
+  if(tokens.method(req,res).toLowerCase() === 'post'){
+    return(ret.concat(JSON.stringify(req.body)))
+  }
+  return (ret)
+  }))
+app.get('/api/persons' , (req,res) => {
+    res.json(persons)
+})
+
+app.get('/api/info',   (req,res) => {
+    const kontsa = `<p> Phonebook has ${persons.length} people</p> <p>${new Date()}</p>`
+    res.send(kontsa)
+})
+
+app.get('/api/persons/:id' , (req,res) => {
+    i = Number(req.params.id)
+    pers = persons.find(p => p.id === i)
+    console.log("pers")
+    if(pers){
+        res.json(pers)
+    }else{
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id' , (req,res) =>{
+    id = Number(req.params.id)
+    persons = persons.filter(per => per.id !== id)
+
+    res.status(204).end()
+})
+
+
+
+app.post('/api/persons' , (req,res) => {
+  const bod = req.body
+  const id = Math.floor(Math.random()*1000000)
+  if(!bod.name){
+    return(res.status(400).json({'error':"nimi puuttuu"}))
+  }else if(!bod.number){
+      return(res.status(400).json({'error':"number puuttuu"}))
+  }else if(persons.find(pers => pers.name === bod.name)){
+      return(res.status(400).json({'error':"nimi löytyy jo!"}))
+  }
+  pers = {
+    name:bod.name,
+    number:bod.number,
+    id:id
+  }
+  persons = persons.concat(pers)
+  res.json(pers)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
